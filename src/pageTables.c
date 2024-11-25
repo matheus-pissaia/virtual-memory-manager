74c445e56eb09e3e76d2df02b86fc11194504aad
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-25 09:51:18 -0300

    feat: page tables (WIP)

---
+++ src/pageTables.c
@@ -0,0 +1,8 @@
+#include "pageTables.h"
+#include "screen.h"
+
+void view_page_tables()
+{
+    clear_screen();
+    // TODO
+}
---
+++ src/pageTables.h
@@ -0,0 +1,6 @@
+#ifndef PAGE_TABLES_H
+#define PAGE_TABLES_H
+
+void view_page_tables();
+
+#endif
