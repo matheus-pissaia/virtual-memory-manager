3b45806404ebcbdd8c4d99a77a34d7362cd4ed93
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-25 01:52:36 -0300

    feat: init memory

---
+++ src/memory.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "memory.h"
+#include "screen.h"
+#include "input.h"
+
+Memory memory;
+
+void init_memory()
+{
+    printf("Configuração inicial:\n");
+    printf("Informe o tamanho da memória física (em bytes): ");
+
+    int memory_size = get_user_input();
+
+    printf("Informe o tamanho do quadro (em bytes): ");
+
+    int frame_size = get_user_input();
+
+    if (memory_size % frame_size != 0)
+    {
+        printf("Erro: O tamanho da memória deve ser múltiplo do tamanho do quadro.\n");
+        exit(1);
+    }
+
+    memory.frame_size = frame_size;
+    memory.total_frames = memory_size / frame_size;
+    memory.frames = (int *)calloc(memory_size, sizeof(int));
+    memory.frame_occupied = (bool *)calloc(memory.total_frames, sizeof(bool));
+    memory.free_frames = memory.total_frames;
+}
+
+void view_memory_usage()
+{
+    clear_screen();
+
+    printf("Memory Usage (%d%% free):\n", (memory.free_frames * 100) / memory.total_frames);
+
+    for (int i = 0; i < memory.total_frames; i++)
+    {
+        printf("Frame %d: %s\n", i, memory.frame_occupied[i] ? "occupied" : "empty");
+    }
+
+    printf("Press any key to go back...");
+    get_user_input();
+}
---
+++ src/memory.h
@@ -0,0 +1,16 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+typedef struct
+{
+    int frame_size;       // Size of each frame (page).
+    int total_frames;     // Total number of frames.
+    int *frames;          // Fisical memory represented by frames.
+    bool *frame_occupied; // Bitmap representing the occupied frames.
+    int free_frames;      // Empty frames counter.
+} Memory;
+
+void init_memory();
+void view_memory_usage();
+
+#endif
