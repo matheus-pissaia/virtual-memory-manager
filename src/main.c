4e4dd8a941afee15fe0366bed35b78088af3ff66
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-25 09:51:04 -0300

    refactor: main menu

--- src/main.c
+++ src/main.c
@@ -1,48 +1,56 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "input.h"
 #include "screen.h"
 #include "windows.h"
+#include "memory.h"
+#include "process.h"
 
-// Function to get user input
-int get_user_input()
+void show_main_menu()
 {
-    int input;
-    scanf("%d", &input);
+    clear_screen();
 
-    return input;
+    printf("Main Menu\n");
+    printf("1. View memory usage\n");
+    printf("2. Create Process\n");
+    printf("3. Show page tables\n");
+    printf("4. Exit program\n");
+    printf("Select an option: ");
 }
 
 int main()
 {
     enable_ansi_on_windows();
+    init_memory();
 
-    clear_screen();
+    int option = 0;
 
-    printf("Main Menu\n");
-    printf("1. Show memory\n");
-    printf("2. Create Process\n");
-    printf("3. Show page tables\n");
-
-    printf("Select an option: ");
-    int input = get_user_input();
-
-    switch (input)
+    do
     {
-    case 1:
-        clear_screen();
-        printf("Option 1 selected\n");
-        break;
-    case 2:
-        clear_screen();
-        printf("Option 2 selected\n");
-        break;
-    case 3:
-        clear_screen();
-        printf("Option 3 selected\n");
-        break;
-    default:
-        printf("Invalid option!\n");
-        break;
-    }
+        show_main_menu();
+
+        option = get_user_input();
+
+        switch (option)
+        {
+        case 1:
+            view_memory_usage();
+            break;
+        case 2:
+            create_process();
+            break;
+        case 3:
+            clear_screen();
+            printf("TODO: Implement page tables\n");
+            break;
+        case 4:
+            printf("Bye bye\n");
+            break;
+        default:
+            printf("Invalid option!\n");
+            break;
+        }
+    } while (option != 4);
 
     return 0;
 }
