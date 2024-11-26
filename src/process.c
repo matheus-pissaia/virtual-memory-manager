c3d5ce71b7167231e8a68fe9f9d0c0f7a111b870
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-26 00:27:33 -0300

    feat: create process structure

--- src/process.c
+++ src/process.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include "memory.h"
 #include "process.h"
+#include "screen.h"
 #include "input.h"
 
 int num_processes = 0;
@@ -8,10 +11,12 @@ Process processes[MAX_PROCESSES];
 
 void create_process()
 {
-    printf("Informe o ID do processo: ");
+    clear_screen();
+
+    printf("Enter the process ID: ");
     int process_id = get_user_input();
 
-    printf("Informe o tamanho do processo (em bytes): ");
+    printf("Enter the size of the process (in bytes): ");
     int process_size = get_user_input();
 
     if (num_processes >= MAX_PROCESSES)
@@ -20,34 +25,36 @@ void create_process()
         return;
     }
 
-    // if (process_size % physical_memory.frame_size != 0)
-    // {
-    //     printf("The process size must be a multiple of the frame size (%d).\n", physical_memory.frame_size);
-    //     return;
-    // }
+    Memory *memory = get_memory();
 
-    // int num_pages = process_size / physical_memory.frame_size;
+    if (process_size % memory->frame_size != 0)
+    {
+        printf("The process size must be a multiple of the frame size (%d).\n", memory->frame_size);
+        return;
+    }
 
-    // if (num_pages > physical_memory.free_frames)
-    // {
-    //     printf("Insufficient memory to alocate the process.\n");
-    //     return;
-    // }
+    int num_pages = process_size / memory->frame_size;
 
-    // Process *p = &processes[num_processes++];
-    // p->process_id = process_id;
-    // p->process_size = process_size;
-    // p->num_pages = num_pages;
-    // p->page_table = (int *)malloc(num_pages * sizeof(int));
+    if (num_pages > memory->free_frames)
+    {
+        printf("Insufficient memory to alocate the process.\n");
+        return;
+    }
 
-    // for (int i = 0, allocated_pages = 0; i < physical_memory.total_frames && allocated_pages < num_pages; i++)
-    // {
-    //     if (!physical_memory.frame_occupied[i])
-    //     {
-    //         physical_memory.frame_occupied[i] = true;
-    //         physical_memory.free_frames--;
-    //         p->page_table[allocated_pages++] = i;
-    //     }
-    // }
-    // printf("Process %d created with %d pages.\n", process_id, num_pages);
+    Process *p = &processes[num_processes++];
+    p->process_id = process_id;
+    p->process_size = process_size;
+    p->num_pages = num_pages;
+    p->page_table = (int *)malloc(num_pages * sizeof(int));
+
+    for (int i = 0, allocated_pages = 0; i < memory->total_frames && allocated_pages < num_pages; i++)
+    {
+        if (!memory->frame_occupied[i])
+        {
+            memory->frame_occupied[i] = true;
+            memory->free_frames--;
+            p->page_table[allocated_pages++] = i;
+        }
+    }
+    printf("Process %d created with %d pages.\n", process_id, num_pages);
 }
